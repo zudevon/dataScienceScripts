b22f9a86d204bfbe9efd52bb675b17978005f19d
DeVon Rasch <devonrasch@live.com>
2018-12-10 04:26:51 -0600

    Update logReg_KNN_Trees.py

--- logReg_KNN_Trees.py
+++ logReg_KNN_Trees.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics, tree, linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from matplotlib import pyplot as plt
+
+
 
 # TEST AND TRAIN VARIABLES
 X = df.iloc[:, 0:10].values
